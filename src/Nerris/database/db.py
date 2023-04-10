aa2cdf37f3fc9271bd709cbddec24d63d4dd6f25
Sapein <5852983+Sapein@users.noreply.github.com>
2023-04-09 21:21:49 -0500

    Remove build, allow file db.

--- src/Nerris/database/db.py
+++ src/Nerris/database/db.py
@@ -3,5 +3,4 @@ from sqlalchemy import create_engine
 def connect(in_memory=True):
     if in_memory:
         return create_engine("sqlite+pysqlite:///:memory:")
-
-    raise NotImplementedError("Only in_memory is supported!")
+    return create_engine("sqlite+pysqlite://nerris_db.sqlite")
