0950cab39e19bb456d2289f148d519fc2322c881
Sapein <5852983+Sapein@users.noreply.github.com>
2024-02-17 17:31:33 -0600

    Add dice rolls and update sqlalchemy version.

---
+++ src/Scout/plugins/dice-rolls.py
@@ -0,0 +1,19 @@
+import discord.ext.commands
+import dice
+
+
+@discord.ext.commands.hybrid_command(name="roll")  # type: ignore
+async def roll(ctx: discord.ext.commands.Context, dice_notation: str):
+    """Rolls a dice using dice notation
+    """
+    try:
+        await ctx.send("Result: {}".format(dice.roll(dice_notation)))
+    except (dice.exceptions.DiceException, dice.exceptions.DiceFatalException) as e:
+        await ctx.send("Unable to roll dice. Reason: ```\n{}```".format(e.pretty_print()))
+
+
+async def setup(bot: discord.ext.commands.Bot):
+    """
+    setup function to make this module into an extension.
+    """
+    bot.add_command(roll)
